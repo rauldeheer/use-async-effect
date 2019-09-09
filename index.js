78da3a4bd46956a0330623f8de7294b5469357cb
Francisco Presencia <franciscop@users.noreply.github.com>
2019-09-09 21:40:46 +0900

    Added `isMounted()`
    Hey great library! I've been doing something similar for a while, but still find issues when I have an async function and then the component might have unmounted after some async state happens. With `isMounted()`, you can check that the component is still mounted before doing anything with the data:
    
    ```js
    const User = () => 
      const [user, setUser] = useState(false);
    
      useAsyncEffect(async isMounted => {
        const data = await fetch(`/users/${id}`).then(res => res.json());
        // Add a check to make sure it's still mounted before updating
        if (!isMounted()) return;
        setUser(data);
      }, [id]);
    
      if (!user) return null;
      return <div>{user.name}</div>;
    };
    ```

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const { useEffect } = require('react');
+const { useEffect, useRef } = require('react');
 
 module.exports.useAsyncEffect = (effect, destroy, inputs) => {
   const hasDestroy = typeof destroy === 'function';
+  const mounted = useRef(true);
 
   useEffect(() => {
     let result;
-    effect().then((value) => result = value);
+    effect(() => mounted.current).then((value) => result = value);
 
-    if (hasDestroy) {
-      return () => destroy(result);
-    }
+    return () => {
+      mounted.current = false;
+      if (hasDestroy) {
+        destroy(result);
+      }
+    };
   }, hasDestroy ? inputs : destroy);
 };
